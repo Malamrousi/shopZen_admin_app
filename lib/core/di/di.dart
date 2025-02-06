3134a7bce0dc087c9cc6cae93a6a785a6a1deb13
MOHAMMEDALAMROU\LENOVO <m.alamrousi.work2@gmail.com>
2025-02-06 12:41:07 +0200

    provide blocs in di

--- lib/core/di/di.dart
+++ lib/core/di/di.dart
@@ -6,42 +6,91 @@ import 'package:shopzen_admin_dashboard/core/cubit/app_cubit/app_cubit.dart';
 import 'package:shopzen_admin_dashboard/core/cubit/upload_image/upload_image_cubit.dart';
 import 'package:shopzen_admin_dashboard/core/upload/data_source/upload_image_data_source.dart';
 import 'package:shopzen_admin_dashboard/core/upload/repo/upload_image_repo.dart';
+import 'package:shopzen_admin_dashboard/features/home/data/data_source/dashboard_data_source.dart';
+import 'package:shopzen_admin_dashboard/features/home/data/repo/dashboard_repo.dart';
+import 'package:shopzen_admin_dashboard/features/home/presentation/bloc/product_number/product_number_bloc.dart';
+import 'package:shopzen_admin_dashboard/features/home/presentation/bloc/users_number/user_number_bloc.dart';
 
 import '../../features/auth/data/data_source/auth_data_source.dart';
 import '../../features/auth/data/repo/auth_repo.dart';
 import '../../features/auth/presentation/bloc/auth_bloc/auth_bloc.dart';
+import '../../features/home/presentation/bloc/category_number/category_number_bloc.dart';
 import '../api/dio_factory.dart';
 
 final getIt = GetIt.instance;
 
-Future<void> setupDependencies() async { 
-   final navKey = GlobalKey<NavigatorState>();
+Future<void> setupDependencies() async {
+  final navKey = GlobalKey<NavigatorState>();
   final sharedPreferences = await SharedPreferences.getInstance();
-    final dio = DioFactory.getDio();
+  final dio = DioFactory.getDio();
 
   getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
- getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
+  getIt.registerLazySingleton<ApiService>(() => ApiService(dio));
   // App Preferences Instance
 
   getIt.registerFactory<AppCubit>(() => AppCubit()); //
-  
-      //UploadImageDataSource
+
+  //UploadImageDataSource
   getIt.registerSingleton<GlobalKey<NavigatorState>>(navKey);
   getIt.registerLazySingleton<UploadImageDataSource>(
-      () => UploadImageDataSource(apiServices: getIt.get<ApiService>()));
-      //UploadImageRepo
-  getIt.registerLazySingleton<UploadImageRepo>(() => UploadImageRepo(
-      uploadImageDataSource: getIt.get<UploadImageDataSource>()));
-      //UploadImageCubit
+    () => UploadImageDataSource(
+      apiServices: getIt.get<ApiService>(),
+    ),
+  );
+  //UploadImageRepo
+  getIt.registerLazySingleton<UploadImageRepo>(
+    () => UploadImageRepo(
+      uploadImageDataSource: getIt.get<UploadImageDataSource>(),
+    ),
+  );
+  //UploadImageCubit
   getIt.registerFactory<UploadImageCubit>(
-      () => UploadImageCubit(uploadImageRepo: getIt.get<UploadImageRepo>()));
+    () => UploadImageCubit(
+      uploadImageRepo: getIt.get<UploadImageRepo>(),
+    ),
+  );
 
-      getIt.registerLazySingleton<AuthDataSource>(
-      () => AuthDataSource(apiService: getIt.get<ApiService>()));
+  getIt.registerLazySingleton<AuthDataSource>(
+    () => AuthDataSource(
+      apiService: getIt.get<ApiService>(),
+    ),
+  );
   getIt.registerLazySingleton<AuthRepo>(
       () => AuthRepo(authDataSource: getIt.get<AuthDataSource>()));
 //LoginBloc
-  getIt
-      .registerFactory<AuthBloc>(() => AuthBloc(repo: getIt.get<AuthRepo>()));
-
+  getIt.registerFactory<AuthBloc>(
+    () => AuthBloc(
+      repo: getIt.get<AuthRepo>(),
+    ),
+  );
+  //DashboardDataSource
+  getIt.registerLazySingleton<DashboardDataSource>(
+    () => DashboardDataSource(
+      apiService: getIt.get<ApiService>(),
+    ),
+  );
+  //DashboardRepo
+  getIt.registerLazySingleton<DashboardRepo>(
+    () => DashboardRepo(
+      dashboardDataSource: getIt.get<DashboardDataSource>(),
+    ),
+  );
+  //CategoryNumberBloc
+  getIt.registerFactory<CategoryNumberBloc>(
+    () => CategoryNumberBloc(
+      getIt.get<DashboardRepo>(),
+    ),
+  );
+  //ProductNumberBloc
+  getIt.registerFactory<ProductNumberBloc>(
+    () => ProductNumberBloc(
+      getIt.get<DashboardRepo>(),
+    ),
+  );
+  //UserNumberBloc
+  getIt.registerFactory<UserNumberBloc>(
+    () => UserNumberBloc(
+      getIt.get<DashboardRepo>(),
+    ),
+  );
 }

6be90c22adcb1fc9470f006abacc639fdb40ac9c
MOHAMMEDALAMROU\LENOVO <m.alamrousi.work2@gmail.com>
2025-02-27 15:22:32 +0200

    add hive package and create hive file

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ import 'package:shopzen_admin_dashboard/core/shared_pref/shared_pref.dart';
 import 'package:shopzen_admin_dashboard/firebase_options.dart';
 import 'package:shopzen_admin_dashboard/shop_zen_admin_dashboard.dart';
 
+import 'core/hive/hive_data_base.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
@@ -17,6 +19,8 @@ void main() async {
   );
   
   await SharedPref().instantiatePreferences();
+
+  await HiveDataBase().init();
 
   await SecureStorageService().instantiateSecureStorage();
 

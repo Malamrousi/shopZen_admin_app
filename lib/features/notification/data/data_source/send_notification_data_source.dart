4701a98143e4b2bb25f14aebe2eea23298aed17d
MOHAMMEDALAMROU\LENOVO <m.alamrousi.work2@gmail.com>
2025-04-11 15:26:27 +0200

    Adds Firebase integration for notifications
    Implements Firebase Cloud Messaging to enable push notifications.
    
    Adds new utility classes for random ID generation and string constants.
    
    Integrates Cloud Firestore to manage notification data.
    
    Updates dependencies and fixes minor UI issues.

--- lib/features/notification/data/data_source/send_notification_data_source.dart
+++ lib/features/notification/data/data_source/send_notification_data_source.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shopzen_admin_dashboard/core/push_notification/firebase_cloud_messaging.dart';
+import 'package:shopzen_admin_dashboard/core/utils/app_string.dart';
+
+import '../../../../core/utils/app_ random_value.dart';
 
 class SendNotificationDataSource {
+  final notificationId = AppRandomValues.randomStringId;
   Future<void> sendNotification(
       {required String title,
       required String body,
@@ -11,5 +16,13 @@ class SendNotificationDataSource {
       productId: productId!,
     );
     return response;
+  }
+
+  Future<void> addNotificationToAllUsersFirebase() async {
+    final user = FirebaseFirestore.instance.collection(userCollection);
+    final userGet = await user.get();
+    for(final docs in userGet.docs) {
+      await user.doc(docs.id).collection(notificationCollection).doc(notificationId).set({});
+    }
   }
 }
